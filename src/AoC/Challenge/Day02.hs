64f0ec41cd2e3761a74c40cccf6e0396e1f7280b
Matthew Green <matgreen@cisco.com>
2022-12-02 11:51:01 +0000

    Tidy up day 2

--- src/AoC/Challenge/Day02.hs
+++ src/AoC/Challenge/Day02.hs
@@ -6,35 +6,32 @@ module AoC.Challenge.Day02 (
 import AoC.Solution
 import Data.Char (ord)
 
-parse :: [Char] -> Either String (Int, Int)
+parse :: String -> Either String (Int, Int)
 parse [a, _, b] = Right (ord a - ord 'A', ord b - ord 'X')
-parse _ = Left "Invalid input"
+parse l = Left $ "Invalid input line: " ++ l
+
+day02 :: ((Int, Int) -> Int) -> ((Int, Int) -> Int) -> Solution [(Int, Int)] Int
+day02 shapeScore outcomeScore =
+  Solution
+    { sParse = traverse parse . lines
+    , sShow = show
+    , sSolve = Right . sum . fmap (\x -> shapeScore x + outcomeScore x)
+    }
 
 shapeScoreA :: (Int, Int) -> Int
 shapeScoreA (_, b) = b + 1
 
 outcomeScoreA :: (Int, Int) -> Int
-outcomeScoreA (a, b) =
-  ((b - a + 1) `mod` 3) * 3
+outcomeScoreA (a, b) = ((b - a + 1) `mod` 3) * 3
 
 day02a :: Solution [(Int, Int)] Int
-day02a =
-  Solution
-    { sParse = traverse parse . lines
-    , sShow = show
-    , sSolve = Right . sum . fmap (\x -> shapeScoreA x + outcomeScoreA x)
-    }
+day02a = day02 shapeScoreA outcomeScoreA
 
 shapeScoreB :: (Int, Int) -> Int
 shapeScoreB (a, b) = ((a + b - 1) `mod` 3) + 1
 
 outcomeScoreB :: (Int, Int) -> Int
-outcomeScoreB (a, b) = b * 3
+outcomeScoreB (_, b) = b * 3
 
 day02b :: Solution [(Int, Int)] Int
-day02b =
-  Solution
-    { sParse = traverse parse . lines
-    , sShow = show
-    , sSolve = Right . sum . fmap (\x -> shapeScoreB x + outcomeScoreB x)
-    }
+day02b = day02 shapeScoreB outcomeScoreB
