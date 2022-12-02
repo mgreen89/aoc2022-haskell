38dc44174d34f7a4c51caa8959d5855fe5f51b19
Matthew Green <matgreen@cisco.com>
2022-12-02 11:44:00 +0000

    Day 2 solution

--- src/AoC/Challenge/Day02.hs
+++ src/AoC/Challenge/Day02.hs
@@ -1,19 +1,40 @@
-{-# LANGUAGE PartialTypeSignatures #-}
-{-# OPTIONS_GHC -Wno-partial-type-signatures #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 module AoC.Challenge.Day02 (
-  )
-where
-
--- day02a
--- , day02b
+  day02a,
+  day02b,
+) where
 
 import AoC.Solution
+import Data.Char (ord)
 
-day02a :: Solution _ _
-day02a = Solution{sParse = Right, sShow = show, sSolve = Right}
+parse :: [Char] -> Either String (Int, Int)
+parse [a, _, b] = Right (ord a - ord 'A', ord b - ord 'X')
+parse _ = Left "Invalid input"
 
-day02b :: Solution _ _
-day02b = Solution{sParse = Right, sShow = show, sSolve = Right}
+shapeScoreA :: (Int, Int) -> Int
+shapeScoreA (_, b) = b + 1
+
+outcomeScoreA :: (Int, Int) -> Int
+outcomeScoreA (a, b) =
+  ((b - a + 1) `mod` 3) * 3
+
+day02a :: Solution [(Int, Int)] Int
+day02a =
+  Solution
+    { sParse = traverse parse . lines
+    , sShow = show
+    , sSolve = Right . sum . fmap (\x -> shapeScoreA x + outcomeScoreA x)
+    }
+
+shapeScoreB :: (Int, Int) -> Int
+shapeScoreB (a, b) = ((a + b - 1) `mod` 3) + 1
+
+outcomeScoreB :: (Int, Int) -> Int
+outcomeScoreB (a, b) = b * 3
+
+day02b :: Solution [(Int, Int)] Int
+day02b =
+  Solution
+    { sParse = traverse parse . lines
+    , sShow = show
+    , sSolve = Right . sum . fmap (\x -> shapeScoreB x + outcomeScoreB x)
+    }
