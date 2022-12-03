836cd64134c51cdfb75f3710c79c89df797b7c35
Matthew Green <matgreen@cisco.com>
2022-12-03 11:54:48 +0000

    Day 3

--- src/AoC/Challenge/Day03.hs
+++ src/AoC/Challenge/Day03.hs
@@ -1,19 +1,40 @@
-{-# LANGUAGE PartialTypeSignatures #-}
-{-# OPTIONS_GHC -Wno-partial-type-signatures #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 module AoC.Challenge.Day03 (
-  )
-where
-
--- day03a
--- , day03b
+  day03a,
+  day03b,
+) where
 
 import AoC.Solution
+import Data.Char (isLower, ord)
+import Data.IntSet (findMin, fromList, intersection)
+import Data.List (foldl1', splitAt)
+import Data.List.Split (chunksOf)
 
-day03a :: Solution _ _
-day03a = Solution{sParse = Right, sShow = show, sSolve = Right}
+prio :: Char -> Int
+prio c
+  | isLower c = ord c - ord 'a' + 1
+  | otherwise = ord c - ord 'A' + 27
 
-day03b :: Solution _ _
-day03b = Solution{sParse = Right, sShow = show, sSolve = Right}
+getCommonPrio :: String -> Int
+getCommonPrio inp =
+  let (a, b) = splitAt (length inp `div` 2) inp
+      (a', b') = (fromList $ prio <$> a, fromList $ prio <$> b)
+   in findMin $ intersection a' b'
+
+day03a :: Solution [String] Int
+day03a =
+  Solution
+    { sParse = Right . lines
+    , sShow = show
+    , sSolve = Right . sum . fmap getCommonPrio
+    }
+
+getCommon :: [String] -> Int
+getCommon = findMin . foldl1' intersection . fmap (fromList . fmap prio)
+
+day03b :: Solution [String] Int
+day03b =
+  Solution
+    { sParse = Right . lines
+    , sShow = show
+    , sSolve = Right . sum . fmap getCommon . chunksOf 3
+    }
