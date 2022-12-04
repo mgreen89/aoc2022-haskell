73cfd5e52318351fdea7848aaceda72735420329
Matthew Green <matgreen@cisco.com>
2022-12-04 12:14:14 +0000

    Tidy up day 4

--- src/AoC/Challenge/Day04.hs
+++ src/AoC/Challenge/Day04.hs
@@ -1,14 +1,10 @@
-{-# LANGUAGE PartialTypeSignatures #-}
-{-# OPTIONS_GHC -Wno-partial-type-signatures #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 module AoC.Challenge.Day04 (
   day04a,
-  day04b
+  day04b,
 ) where
 
 import AoC.Solution
+import AoC.Util (maybeToEither)
 import Control.Monad ((<=<))
 import Data.List.Split (splitOn)
 import Text.Read (readMaybe)
@@ -17,38 +13,32 @@ listTup2 :: [a] -> Maybe (a, a)
 listTup2 [x, y] = Just (x, y)
 listTup2 _ = Nothing
 
-parseLine :: String -> Maybe ((Int, Int), (Int, Int))
-parseLine l = do
-  (f, s) <- listTup2 . splitOn "," $ l
-  (a, b) <- listTup2 <=< traverse readMaybe . splitOn "-" $ f
-  (c, d) <- listTup2 <=< traverse readMaybe . splitOn "-" $ s
-  pure ((a, b), (c, d))
+type Areas = ((Int, Int), (Int, Int))
 
-checkA :: ((Int, Int), (Int, Int)) -> Bool
+parseLine :: String -> Maybe Areas
+parseLine =
+  listTup2
+    <=< traverse (listTup2 <=< traverse readMaybe . splitOn "-")
+      . splitOn ","
+
+day04 :: (Areas -> Bool) -> Solution [Areas] Int
+day04 check =
+  Solution
+    { sParse = maybeToEither "Invalid input" . traverse parseLine . lines
+    , sShow = show
+    , sSolve = Right . length . filter id . fmap check
+    }
+
+checkA :: Areas -> Bool
 checkA ((a, b), (c, d)) =
   a <= c && b >= d || a >= c && b <= d
 
-run :: (((Int, Int), (Int, Int)) -> Bool) -> [String] -> Either String Int
-run check ls = case traverse parseLine ls of
-  Just l -> Right . length . filter id . fmap check $ l
-  Nothing -> Left "Invalid line"
+day04a :: Solution [Areas] Int
+day04a = day04 checkA
 
-day04a :: Solution [String] Int
-day04a =
-  Solution
-    { sParse = Right . lines
-    , sShow = show
-    , sSolve = run checkA
-    }
-
-checkB :: ((Int, Int), (Int, Int)) -> Bool
+checkB :: Areas -> Bool
 checkB ((a, b), (c, d)) =
   a <= c && b >= c || a <= d && b >= d || c <= a && d >= a || c <= b && d >= b
 
-day04b :: Solution [String] Int
-day04b =
-  Solution
-    { sParse = Right . lines
-    , sShow = show
-    , sSolve = run checkB
-    }
+day04b :: Solution [Areas] Int
+day04b = day04 checkB
