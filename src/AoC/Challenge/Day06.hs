81675b034bb3c02ad372f2a40cb0744d3880d30f
Matthew Green <matgreen@cisco.com>
2022-12-06 10:41:42 +0000

    Day 6 (bit icky)

--- src/AoC/Challenge/Day06.hs
+++ src/AoC/Challenge/Day06.hs
@@ -1,19 +1,32 @@
-{-# LANGUAGE PartialTypeSignatures #-}
-{-# OPTIONS_GHC -Wno-partial-type-signatures #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 module AoC.Challenge.Day06 (
-  )
+  day06a,
+  day06b
+)
 where
 
--- day06a
--- , day06b
-
 import AoC.Solution
+import AoC.Util (maybeToEither)
+import Control.Applicative (getZipList, ZipList(..))
+import Data.List (zip4)
 
-day06a :: Solution _ _
-day06a = Solution{sParse = Right, sShow = show, sSolve = Right}
+solve :: Int -> String -> Either String Int
+solve n s =
+  let f = foldr check (Right n) . reverse . getZipList . traverse ZipList . take n . iterate (drop 1) $ s
+  in case f of
+    Right c -> Left "Could not find 4 different"
+    Left i -> Right i
+ where
+  check :: String -> Either Int Int -> Either Int Int
+  check _ (Left i) = Left i
+  check s (Right c) = if allDifferent s then Left c else Right (c + 1)
 
-day06b :: Solution _ _
-day06b = Solution{sParse = Right, sShow = show, sSolve = Right}
+  allDifferent :: (Eq a) => [a] -> Bool
+  allDifferent = \case
+    [] -> True
+    (x : xs) -> x `notElem` xs && allDifferent xs
+
+day06a :: Solution String Int
+day06a = Solution{sParse = Right, sShow = show, sSolve = solve 4}
+
+day06b :: Solution String Int
+day06b = Solution{sParse = Right, sShow = show, sSolve = solve 14}
