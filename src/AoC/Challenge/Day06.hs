e142148d989273d9da8127cde4e1807e8b0de81e
Matthew Green <matgreen@cisco.com>
2022-12-06 11:37:41 +0000

    Day 6 further tidy

--- src/AoC/Challenge/Day06.hs
+++ src/AoC/Challenge/Day06.hs
@@ -6,27 +6,25 @@ where
 
 import AoC.Solution
 import AoC.Util (maybeToEither)
-import Control.Applicative (getZipList, ZipList(..))
 import Control.Monad (foldM)
 import Data.List (tails)
+import qualified Data.Set as S
+import Text.Printf (printf)
+
+allDifferent :: (Ord a) => [a] -> Bool
+allDifferent x = length x == S.size (S.fromList x)
 
 windows :: Int -> [a] -> [[a]]
-windows n = getZipList . traverse ZipList . take n . tails
+windows n = foldr (zipWith (:)) (repeat []) . take n . tails
 
 solve :: Int -> String -> Either String Int
 solve n s =
-  let f = foldM check n $ windows n s
-  in case f of
-    Right c -> Left "Could not find 4 different"
+  case foldM check n (windows n s) of
+    Right c -> Left $ printf "Could not find %d different" n
     Left i -> Right i
  where
   check :: Int -> String -> Either Int Int
   check c s = if allDifferent s then Left c else Right (c + 1)
-
-  allDifferent :: (Eq a) => [a] -> Bool
-  allDifferent = \case
-    [] -> True
-    (x : xs) -> x `notElem` xs && allDifferent xs
 
 day06a :: Solution String Int
 day06a = Solution{sParse = Right, sShow = show, sSolve = solve 4}
