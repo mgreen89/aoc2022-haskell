0c92c13208d86d3ef8f5ba74afdb954ff1a5c29d
Matthew Green <matgreen@cisco.com>
2022-12-19 10:40:38 +0000

    Day 19a (parsing only)

--- src/AoC/Challenge/Day19.hs
+++ src/AoC/Challenge/Day19.hs
@@ -1,19 +1,64 @@
-{-# LANGUAGE PartialTypeSignatures #-}
-{-# OPTIONS_GHC -Wno-partial-type-signatures #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 module AoC.Challenge.Day19 (
-  )
+  day19a,
+  day19b,
+)
 where
 
--- day19a
--- , day19b
-
 import AoC.Solution
+import Control.DeepSeq (NFData)
+import Data.Bifunctor (first)
+import Data.Void (Void)
+import GHC.Generics (Generic)
+import qualified Text.Megaparsec as MP
+import qualified Text.Megaparsec.Char as MP
+import qualified Text.Megaparsec.Char.Lexer as MPL
 
-day19a :: Solution _ _
-day19a = Solution{sParse = Right, sShow = show, sSolve = Right}
+data Resource = Ore | Clay | Obsidian deriving (Show, Generic, NFData)
 
-day19b :: Solution _ _
-day19b = Solution{sParse = Right, sShow = show, sSolve = Right}
+type Recipe = [(Resource, Int)]
+
+data Blueprint = Blueprint
+  { ore :: Recipe
+  , clay :: Recipe
+  , obsidian :: Recipe
+  , geode :: Recipe
+  }
+  deriving (Show, Generic, NFData)
+
+blueprintParser :: MP.Parsec Void String (Int, Blueprint)
+blueprintParser = do
+  MP.string "Blueprint "
+  i <- MPL.decimal <* MP.string ":" <* MP.space
+  MP.string "Each ore robot costs "
+  ore <- recipe <* MP.string "." <* MP.space
+  MP.string "Each clay robot costs "
+  clay <- recipe <* MP.string "." <* MP.space
+  MP.string "Each obsidian robot costs "
+  obsidian <- recipe <* MP.string "." <* MP.space
+  MP.string "Each geode robot costs "
+  geode <- recipe <* MP.string "."
+  pure (i, Blueprint{..})
+ where
+  recipe :: MP.Parsec Void String Recipe
+  recipe =
+    flip MP.sepBy (MP.string " and ") $ do
+      c <- MPL.decimal <* MP.space
+      r <- resource
+      pure (r, c)
+  resource :: MP.Parsec Void String Resource
+  resource =
+    MP.choice
+      [ Ore <$ MP.string "ore"
+      , Clay <$ MP.string "clay"
+      , Obsidian <$ MP.string "obsidian"
+      ]
+
+parse :: String -> Either String [(Int, Blueprint)]
+parse =
+  first MP.errorBundlePretty . MP.parse (MP.sepBy blueprintParser MP.space) "day19"
+
+day19a :: Solution [(Int, Blueprint)] Int
+day19a = Solution{sParse = parse, sShow = show, sSolve = Left . show}
+
+day19b :: Solution [(Int, Blueprint)] Int
+day19b = Solution{sParse = parse, sShow = show, sSolve = Left . const "not implemented"}
