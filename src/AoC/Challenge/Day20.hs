38ba2d3a5bbbc1041a17ec838488bd14f9c3dc09
Matthew Green <matgreen@cisco.com>
2022-12-20 20:14:43 +0000

    Day 20

--- src/AoC/Challenge/Day20.hs
+++ src/AoC/Challenge/Day20.hs
@@ -1,19 +1,80 @@
-{-# LANGUAGE PartialTypeSignatures #-}
-{-# OPTIONS_GHC -Wno-partial-type-signatures #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 module AoC.Challenge.Day20 (
-  )
+  day20a,
+  day20b,
+)
 where
 
--- day20a
--- , day20b
-
 import AoC.Solution
+import Control.Monad (foldM, forM_, when)
+import Control.Monad.Primitive (PrimMonad, PrimState)
+import Control.Monad.ST (runST)
+import Control.Monad.Trans.Except (runExceptT, throwE)
+import Data.Foldable (traverse_)
+import qualified Data.Vector as V
+import Data.Vector.Mutable (MVector)
+import qualified Data.Vector.Mutable as MV
+import Text.Read (readEither)
 
-day20a :: Solution _ _
-day20a = Solution{sParse = Right, sShow = show, sSolve = Right}
+findIndex :: (PrimMonad m) => (a -> Bool) -> MVector (PrimState m) a -> m Int
+findIndex check v = do
+  x <- runExceptT $ foldM go () [0 .. MV.length v - 1]
+  case x of
+    Left i -> pure i
+    Right _ -> error "didn't find index"
+ where
+  go _ i = do
+    c <- MV.read v i
+    when (check c) $ throwE i
 
-day20b :: Solution _ _
-day20b = Solution{sParse = Right, sShow = show, sSolve = Right}
+mix :: (PrimMonad m) => MVector (PrimState m) (Int, Int) -> m ()
+mix v =
+  forM_
+    [0 .. MV.length v - 1]
+    $ \w -> do
+      i <- findIndex ((== w) . fst) v
+      a@(_, x) <- MV.read v i
+      let j = (i + x) `mod` (MV.length v - 1)
+      case compare i j of
+        LT -> MV.move (MV.slice i (j - i) v) (MV.slice (i + 1) (j - i) v)
+        GT -> MV.move (MV.slice (j + 1) (i - j) v) (MV.slice j (i - j) v)
+        EQ -> pure ()
+      MV.write v j a
+
+coords :: (PrimMonad m) => MVector (PrimState m) (Int, Int) -> m Int
+coords v = do
+  start <- findIndex ((== 0) . snd) v
+  cs <-
+    traverse
+      (\i -> snd <$> MV.read v ((start + i) `mod` MV.length v))
+      [1000, 2000, 3000]
+  pure $ sum cs
+
+solveA :: [Int] -> Int
+solveA inp =
+  runST $ do
+    v <- V.thaw . V.fromList . zip [0 ..] $ inp
+    mix v
+    coords v
+
+day20a :: Solution [Int] Int
+day20a =
+  Solution
+    { sParse = traverse readEither . lines
+    , sShow = show
+    , sSolve = Right . solveA
+    }
+
+solveB :: Int -> [Int] -> Int
+solveB key inp =
+  runST $ do
+    v <- V.thaw . V.fromList . zip [0 ..] . fmap (* key) $ inp
+    traverse_ (const $ mix v) ([1 .. 10] :: [Int])
+    coords v
+
+day20b :: Solution [Int] Int
+day20b =
+  Solution
+    { sParse = traverse readEither . lines
+    , sShow = show
+    , sSolve = Right . solveB 811589153
+    }
