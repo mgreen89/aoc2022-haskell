43ff78ed0ba811292b0518f0b78a51e17c81b1de
Matthew Green <matgreen@cisco.com>
2022-12-21 09:49:56 +0000

    Day 21 a

--- src/AoC/Challenge/Day21.hs
+++ src/AoC/Challenge/Day21.hs
@@ -1,19 +1,43 @@
-{-# LANGUAGE PartialTypeSignatures #-}
-{-# OPTIONS_GHC -Wno-partial-type-signatures #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 module AoC.Challenge.Day21 (
-  )
+  day21a,
+  day21b,
+)
 where
 
--- day21a
--- , day21b
-
 import AoC.Solution
+import Data.Map (Map)
+import qualified Data.Map as M
 
-day21a :: Solution _ _
-day21a = Solution{sParse = Right, sShow = show, sSolve = Right}
+toOp :: String -> (Int -> Int -> Int)
+toOp "+" = (+)
+toOp "-" = (-)
+toOp "/" = div
+toOp "*" = (*)
+toOp x = error $ "Invalid op: " ++ x
 
-day21b :: Solution _ _
-day21b = Solution{sParse = Right, sShow = show, sSolve = Right}
+parse :: Map String Int -> [String] -> (String, Int)
+parse _ [m, v] = (init m, read v)
+parse ms [m, a, o, b] = (init m, toOp o (ms M.! a) (ms M.! b))
+parse _ x = error $ "Invalid parse: " ++ unwords x
+
+solveA :: [String] -> Int
+solveA inp = ms M.! "root"
+ where
+  -- Recursive definition - just let Haskell sort it out.
+  ms = M.fromList . fmap (parse ms . words) $ inp
+
+day21a :: Solution [String] Int
+day21a =
+  Solution
+    { sParse = Right . lines
+    , sShow = show
+    , sSolve = Right . solveA
+    }
+
+day21b :: Solution [String] String
+day21b =
+  Solution
+    { sParse = Right . lines
+    , sShow = show
+    , sSolve = Left . const "not implemented"
+    }
