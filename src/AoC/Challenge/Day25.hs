03733462ab55ad6d8fc356f69557aa604fbb7a76
Matthew Green <matgreen@cisco.com>
2022-12-25 10:38:37 +0000

    Day 25 - Merry Christmas!

--- src/AoC/Challenge/Day25.hs
+++ src/AoC/Challenge/Day25.hs
@@ -1,19 +1,47 @@
-{-# LANGUAGE PartialTypeSignatures #-}
-{-# OPTIONS_GHC -Wno-partial-type-signatures #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 module AoC.Challenge.Day25 (
-  )
+  day25a,
+)
 where
 
--- day25a
--- , day25b
-
 import AoC.Solution
+import AoC.Util (maybeToEither)
+import Control.Applicative (liftA2)
+import Data.List (unfoldr)
 
-day25a :: Solution _ _
-day25a = Solution{sParse = Right, sShow = show, sSolve = Right}
+snafuToInt :: String -> Maybe Int
+snafuToInt =
+  foldl (\a c -> liftA2 (+) (charToInt c) (fmap (* 5) a)) (Just 0)
+ where
+  charToInt = \case
+    '=' -> Just (-2)
+    '-' -> Just (-1)
+    '0' -> Just 0
+    '1' -> Just 1
+    '2' -> Just 2
+    _ -> Nothing
 
-day25b :: Solution _ _
-day25b = Solution{sParse = Right, sShow = show, sSolve = Right}
+intToSnafu :: Int -> String
+intToSnafu =
+  reverse . unfoldr go
+ where
+  go :: Int -> Maybe (Char, Int)
+  go 0 = Nothing
+  go i =
+    Just $ case i `mod` 5 of
+      0 -> ('0', i `div` 5)
+      1 -> ('1', i `div` 5)
+      2 -> ('2', i `div` 5)
+      3 -> ('=', (i `div` 5) + 1)
+      4 -> ('-', (i `div` 5) + 1)
+      _ -> error "Mod function"
+
+day25a :: Solution [String] String
+day25a =
+  Solution
+    { sParse = Right . lines
+    , sShow = id
+    , sSolve =
+        fmap (intToSnafu . sum)
+          . maybeToEither "Invalid snafu number"
+          . traverse snafuToInt
+    }
