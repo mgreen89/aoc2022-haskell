3e3cfd68b37820b3c8a8757d898945ebcb423071
Matthew Green <matgreen@cisco.com>
2022-12-29 15:04:50 +0000

    Speed up day 24 using BFS instead of A*

--- src/AoC/Challenge/Day24.hs
+++ src/AoC/Challenge/Day24.hs
@@ -4,20 +4,21 @@ module AoC.Challenge.Day24 (
 )
 where
 
-import AoC.Common.Graph (aStar)
-import AoC.Common.Point (boundingBox', cardinalNeighbs, inBoundingBox, manhattan)
+import AoC.Common.Point (boundingBox', cardinalNeighbs, inBoundingBox)
 import AoC.Solution
 import Data.Array (Array)
 import qualified Data.Array as A
-import Data.Map (Map)
 import qualified Data.Map as M
+import Data.Set (Set)
+import qualified Data.Set as S
 import Linear (V2 (..))
 
 type Point = V2 Int
 
-type Blizzards = Array Point Bool
+type DirBlizzards = Array Point Bool
+type Blizzards = (DirBlizzards, DirBlizzards, DirBlizzards, DirBlizzards)
 
-type Input = (Point, Point, Blizzards, Blizzards, Blizzards, Blizzards)
+type Input = (Point, Point, Blizzards)
 
 parseInput :: String -> Input
 parseInput inp =
@@ -37,13 +38,15 @@ parseInput inp =
    in
     ( fst . head . M.toList . M.filterWithKey (\(V2 _ y) a -> y == yLo && a == '.') $ initial
     , fst . head . M.toList . M.filterWithKey (\(V2 _ y) a -> y == yHi && a == '.') $ initial
-    , A.array (V2 0 0, V2 (xHi - 1) (yHi - 1)) $ fmap (fmap (== '^')) noWalls
-    , A.array (V2 0 0, V2 (xHi - 1) (yHi - 1)) $ fmap (fmap (== '>')) noWalls
-    , A.array (V2 0 0, V2 (xHi - 1) (yHi - 1)) $ fmap (fmap (== 'v')) noWalls
-    , A.array (V2 0 0, V2 (xHi - 1) (yHi - 1)) $ fmap (fmap (== '<')) noWalls
+    ,
+      ( A.array (V2 0 0, V2 (xHi - 1) (yHi - 1)) $ fmap (fmap (== '^')) noWalls
+      , A.array (V2 0 0, V2 (xHi - 1) (yHi - 1)) $ fmap (fmap (== '>')) noWalls
+      , A.array (V2 0 0, V2 (xHi - 1) (yHi - 1)) $ fmap (fmap (== 'v')) noWalls
+      , A.array (V2 0 0, V2 (xHi - 1) (yHi - 1)) $ fmap (fmap (== '<')) noWalls
+      )
     )
 
-isBlizzard :: (Blizzards, Blizzards, Blizzards, Blizzards) -> Int -> Point -> Bool
+isBlizzard :: Blizzards -> Int -> Point -> Bool
 isBlizzard (u, r, d, l) turn (V2 x y) =
   inU || inR || inD || inL
  where
@@ -53,72 +56,43 @@ isBlizzard (u, r, d, l) turn (V2 x y) =
   inD = d A.! V2 x ((y - turn) `mod` (yHi + 1))
   inL = l A.! V2 ((x + turn) `mod` (xHi + 1)) y
 
-solveA :: Input -> Int
-solveA (start, dest, u, r, d, l) =
-  case aStar heuristic neighbs (start, 0) ((== dest) . fst) of
-    Just x -> x
-    Nothing -> error "no path!"
+bfs :: Int -> Input -> Int
+bfs startTime (start, dest, blizzards@(u, _, _, _)) =
+  go startTime (S.fromList [start])
  where
-  (lo, hi) = A.bounds u
+  go :: Int -> Set Point -> Int
+  go t ps
+    | dest `S.member` ps = t
+    | otherwise =
+        go (t + 1)
+          . S.foldl (\s -> S.union s . neighbs t) S.empty
+          $ ps
 
-  neighbs :: (Point, Int) -> Map (Point, Int) Int
-  neighbs (n, t) =
-    M.fromList
-      . flip zip (repeat 1)
-      . flip zip (repeat (t + 1))
+  neighbs :: Int -> Point -> Set Point
+  neighbs t p =
+    S.fromList
       . filter (isValid (t + 1))
-      $ n : cardinalNeighbs n
+      $ p : cardinalNeighbs p
+
+  (lo, hi) = A.bounds u
 
   isValid :: Int -> Point -> Bool
   isValid t p
     | p == start = True
     | p == dest = True
     | otherwise =
-        inBoundingBox (lo, hi) p && not (isBlizzard (u, r, d, l) t p)
-
-  heuristic :: (Point, Int) -> Int
-  heuristic (n, t) =
-    t + manhattan n dest
+        inBoundingBox (lo, hi) p && not (isBlizzard blizzards t p)
 
 day24a :: Solution Input Int
-day24a = Solution{sParse = Right . parseInput, sShow = show, sSolve = Right . solveA}
+day24a = Solution{sParse = Right . parseInput, sShow = show, sSolve = Right . bfs 0}
 
 solveB :: Input -> Int
-solveB (start, dest, u, r, d, l) =
-  timeThere + timeBack + timeThereAgain
+solveB (start, dest, blizzards) =
+  timeThereAgain
  where
-  (lo, hi) = A.bounds u
-  neighbs (n, t) =
-    M.fromList
-      . flip zip (repeat 1)
-      . flip zip (repeat (t + 1))
-      . filter (isValid (t + 1))
-      $ n : cardinalNeighbs n
-
-  heuristic (n, t) =
-    t + manhattan n dest
-
-  isValid :: Int -> Point -> Bool
-  isValid t p
-    | p == start = True
-    | p == dest = True
-    | otherwise =
-        inBoundingBox (lo, hi) p && not (isBlizzard (u, r, d, l) t p)
-
-  isStart = (== start) . fst
-  isDest = (== dest) . fst
-
-  timeThere = case aStar heuristic neighbs (start, 0) isDest of
-    Just x -> x
-    Nothing -> error "no path there"
-
-  timeBack = case aStar heuristic neighbs (dest, timeThere) isStart of
-    Just x -> x
-    Nothing -> error "no path back"
-
-  timeThereAgain = case aStar heuristic neighbs (start, timeThere + timeBack) isDest of
-    Just x -> x
-    Nothing -> error "no path there again"
+  timeThere = bfs 0 (start, dest, blizzards)
+  timeBack = bfs timeThere (dest, start, blizzards)
+  timeThereAgain = bfs timeBack (start, dest, blizzards)
 
 day24b :: Solution Input Int
 day24b = Solution{sParse = Right . parseInput, sShow = show, sSolve = Right . solveB}
